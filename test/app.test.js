4dbc323d9fe6389057916e8ced9e6c501351255b
Antoine-Goncalves <goncalves.devweb@gmail.com>
2019-12-04 10:11:46 +0100

    :construction: add another test in app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -4,6 +4,7 @@ const app = require("../app");
 
 const sumResult1 = app.sum(5, 5);
 const sumResult2 = app.sum(-10, 10);
+const sumResult3 = app.sum(2, 4);
 
 describe("App", function() {
   describe("sum()", function() {
@@ -12,6 +13,9 @@ describe("App", function() {
     });
     it("sum should return 0", function() {
       assert.equal(sumResult2, 0);
+    });
+    it("sum should be a number types", function() {
+      assert.typeOf(sumResult3, "number");
     });
   });
 });
