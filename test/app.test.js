e21021bfffcc585ea88986e213df0e7682f306c0
Antoine-Goncalves <goncalves.devweb@gmail.com>
2019-12-04 09:55:22 +0100

    :white_check_mark: create test for sum function

--- test/app.test.js
+++ test/app.test.js
@@ -3,3 +3,11 @@ const assert = require("chai").assert;
 const app = require("../app");
 
 const sumResult1 = app.sum(5, 5);
+
+describe("App", function() {
+  describe("sum()", function() {
+    it("sum should return 10", function() {
+      assert.equal(sumResult1, 10);
+    });
+  });
+});
