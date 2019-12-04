386d94628bf759bd68cb5d8b142dd3d25618fc10
Antoine-Goncalves <goncalves.devweb@gmail.com>
2019-12-04 10:01:28 +0100

    :construction: create another test

--- test/app.test.js
+++ test/app.test.js
@@ -3,11 +3,15 @@ const assert = require("chai").assert;
 const app = require("../app");
 
 const sumResult1 = app.sum(5, 5);
+const sumResult2 = app.sum(-10, 10);
 
 describe("App", function() {
   describe("sum()", function() {
     it("sum should return 10", function() {
       assert.equal(sumResult1, 10);
+    });
+    it("sum should return 0", function() {
+      assert.equal(sumResult2, 0);
     });
   });
 });
